d47e4fe7f4d80b0c8247d9f3931f47c24f50f423
Jonathan Skaggs <jbskaggs12@gmail.com>
2022-06-20 19:22:48 -0600

    initial commit

---
+++ CppDemo.cpp
@@ -0,0 +1,41 @@
+//
+// Created by jskag on 6/20/2022.
+//
+
+#include <iostream>
+#include <fstream>  // (fstream stands for file stream)
+#include <string>
+#include <sstream>
+#include <list>
+
+using namespace std;
+
+//// print string function
+
+
+int main(int argc, char *argv[]) {
+////    How do you run the code?
+
+////    What are command-line arguments?
+
+
+////    How do you add the strings to a list?
+
+
+////    How do you sort the list?
+
+
+////    How do you read strings from a text file?
+
+
+////    How do you write the list to a text file?
+
+
+////    How do you convert from an int to a string?
+
+
+////    How do you convert from a string to an int?
+
+
+    return 0;
+}
